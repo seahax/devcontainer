9425e9653846710ca2587a792da2a46904f28b0f
Chris Ackerman <chris@topher.land>
2025-08-09 18:23:17 +0000

    improve bootstrap script mise cache and terminal output

--- dotfiles/.bootstrap.zsh
+++ dotfiles/.bootstrap.zsh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 
+export TERM=dumb
+mkdir -p /mnt/home/.cache/mise
+rm -rf $HOME/.cache/mise
+ln -s -t ~/.cache /mnt/home/.cache/mise
 mise trust --yes --all
+mise use --yes --global usage
 mise install --yes
-mise run --continue-on-error --jobs=1 --no-cache "init:**"
+mise run --continue-on-error --no-cache "init:**"
 true
